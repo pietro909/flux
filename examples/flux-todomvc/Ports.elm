d81499940ca3f6de07022ae1414647654b0aa7a6
Pietro Grandi <pietro.grandi@betsson.com>
2016-06-10 11:49:06 +0200

    compiling!

--- examples/flux-todomvc/Ports.elm
+++ examples/flux-todomvc/Ports.elm
@@ -10,3 +10,18 @@ port dispatchDestroyCompleted : ({} -> msg) -> Sub msg
 port dispatchToggleCompleteAll : ({} -> msg) -> Sub msg
 port dispatchUndoComplete : (TodoId -> msg) -> Sub msg
 port dispatchUpdateText : ((TodoId, String) -> msg) -> Sub msg
+
+
+{-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ dispatchCreate Create
+        , dispatchComplete Complete
+        , dispatchDestroy Destroy
+        , dispatchDestroyCompleted (always DestroyCompleted)
+        , dispatchToggleCompleteAll (always toggleCompleteAll)
+        , dispatchUndocomplete UndoComplete
+        , dispatchUpdateText (uncurry UpdateText)
+        ]
+        --}
--- examples/flux-todomvc/TodoStore.elm
+++ examples/flux-todomvc/TodoStore.elm
@@ -1,14 +1,28 @@
 module TodoStore exposing (..)
 
-import Ports
+import Ports 
 import Json.Decode
 import TypeAliases exposing (TodoId)
+
+{--
+(
+        dispatchCreate, 
+        dispatchComplete,
+        dispatchDestroy, 
+        dispatchDestroyCompleted,
+        dispatchToggleCompleteAll,
+        dispatchUndocomplete,
+        dispatchUpdateText 
+        )
+--}
 
 -- here it starts!
 
 -- MODEL
-
--- UPDATE
+type alias Model = 
+    { todos : List String
+    , uid : Int
+    }
 
 {-- port constants from TodoConstants.js to Elm Actions --}
 type Action
@@ -19,3 +33,17 @@ type Action
     | ToggleCompleteAll
     | UndoComplete TodoId
     | UpdateText TodoId String
+
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Action
+subscriptions model =
+    Sub.batch
+        [ Ports.dispatchCreate Create
+        , Ports.dispatchComplete Complete
+        , Ports.dispatchDestroy Destroy
+        , Ports.dispatchDestroyCompleted (always DestroyCompleted)
+        , Ports.dispatchToggleCompleteAll (always ToggleCompleteAll)
+        , Ports.dispatchUndoComplete UndoComplete
+        , Ports.dispatchUpdateText (uncurry UpdateText)
+        ]
